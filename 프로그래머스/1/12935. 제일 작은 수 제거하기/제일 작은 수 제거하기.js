6320c0d9eb2aa80dc7b96c900bdc5599adcf338c
ydsic <ydsic99@gmail.com>
2025-03-10 23:41:01 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.45 ms, Memory: 43.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+    if(arr.length === 1) {
+        return [-1];
+    } else {
+        const min = Math.min(...arr);
+        return arr.filter(num => num !== min);
+    }
+}
