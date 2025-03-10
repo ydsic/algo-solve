07bad44cb43131545d0370d78c6279142cd46cc5
ydsic <ydsic99@gmail.com>
2025-03-10 23:32:20 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,8 +1,8 @@
 function solution(n) {
-    let a = 0;  // 변수 초기화
+    let a = 0; 
     while (n > 0) {
-        a += n % 10;  // 마지막 자리 숫자 더하기
-        n = Math.floor(n / 10);  // 마지막 자리 제거 (소수점 방지)
+        a += n % 10; 
+        n = Math.floor(n / 10);  
     }
     return a;
 }
