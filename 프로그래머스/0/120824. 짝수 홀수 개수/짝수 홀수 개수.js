b81e226233f1b6149ca96badd02a7a4e0a17fb43
ydsic <ydsic99@gmail.com>
2025-03-10 23:30:55 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    let a = [0, 0];
+    for(let i = 0; i < num_list.length; i++){
+        if(num_list[i] % 2 == 0){
+            a[0]++;
+        } else {
+            a[1]++;
+        }
+    }
+    return a;
+}
