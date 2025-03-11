ae7763c08b7e92090304654e73a3376be4e567e8
ydsic <ydsic99@gmail.com>
2025-03-11 13:19:29 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    s = s.toLowerCase().split(' ');
+    
+    for(let i in s){
+        if(s[i]){
+            s[i] = s[i][0].toUpperCase() + s[i].slice(1);
+        }
+    }
+    return s.join(" ")
+}
